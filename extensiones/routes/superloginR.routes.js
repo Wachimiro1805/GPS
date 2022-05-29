b176bc3ca824dc2ae28eb3f45a9bfacd48ebe493
DiegoCadena <diego@graywolf.co>
2022-05-28 18:11:27 -0600

    consolidar html

--- extensiones/routes/superloginR.routes.js
+++
@@ -1,8 +0,0 @@
-import Router from 'express';
-import { consultarLogin } from '../modelos/superloginRegistro.model.js';
-
-const router = Router();
-
-router.post('/login',consultarLogin);
-
-export default router; 
